f3ba13d2310464a78b663d33d10f6810c40f410e
Thiago Victorino <thiagovictorino@Thiagos-MacBook-Pro.local>
2016-06-30 16:00:45 -0300

    Change error handler

--- src/Adapter/GuzzleHttpAdapter.php
+++ src/Adapter/GuzzleHttpAdapter.php
@@ -162,6 +162,8 @@ class GuzzleHttpAdapter implements AdapterInterface
 
         $content = json_decode($body);
 
-        throw new HttpException(isset($content->message) ? $content->message : 'Request not processed.', $code);
+        $error = $content->errors[0];
+
+        throw new HttpException(isset($error) ? $error->description : 'Request not processed.', $code);
     }
 }
